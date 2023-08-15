6e42d3a1878993e799f7b03f722c6e79c2c11046
vchs <vchs1710@yandex.ru>
2023-08-15 16:08:36 +0300

    maximum profit achieve from this transaction

---
+++ src/test/java/ru/chernov/easy/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,24 @@
+package ru.chernov.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BestTimeToBuyAndSellStockTest {
+
+    private final BestTimeToBuyAndSellStock bestTimeToBuyAndSellStock = new BestTimeToBuyAndSellStock();
+
+    @Test
+    void test1() {
+        int result = bestTimeToBuyAndSellStock.maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+
+        assertEquals(5, result);
+    }
+
+    @Test
+    void test2() {
+        int result = bestTimeToBuyAndSellStock.maxProfit(new int[]{7, 6, 4, 3, 1});
+
+        assertEquals(0, result);
+    }
+}
