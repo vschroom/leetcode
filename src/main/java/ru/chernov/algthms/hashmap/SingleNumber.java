3d1b69d5c6df6ce448d56653b1cb78f97c50f549
vchs <vchs1710@yandex.ru>
2024-10-17 10:21:27 +0300

    single number

---
+++ src/main/java/ru/chernov/algthms/hashmap/SingleNumber.java
@@ -0,0 +1,35 @@
+package ru.chernov.algthms.hashmap;
+
+import java.util.HashSet;
+
+/**
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ */
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (var el : nums) {
+            res ^= el;
+        }
+
+        return res;
+    }
+
+    public int singleNumber2(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (var el : nums) {
+            if (set.contains(el)) {
+                set.remove(el);
+            } else {
+                set.add(el);
+            }
+        }
+
+        for (var el : set) {
+            return el;
+        }
+
+        return 0;
+    }
+}
---
+++ src/test/java/ru/chernov/hashmap/SingleNumberTest.java
@@ -0,0 +1,24 @@
+package ru.chernov.hashmap;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.chernov.algthms.hashmap.SingleNumber;
+
+class SingleNumberTest {
+
+    private final SingleNumber singleNumber = new SingleNumber();
+
+    @Test
+    void test1() {
+        int result = singleNumber.singleNumber(new int[]{4, 1, 2, 1, 2});
+
+        Assertions.assertEquals(4, result);
+    }
+
+    @Test
+    void test2() {
+        int result = singleNumber.singleNumber2(new int[]{4, 1, 2, 1, 2});
+
+        Assertions.assertEquals(4, result);
+    }
+}
