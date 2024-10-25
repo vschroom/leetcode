bf96b18f2338cccc802090543ec07c438cf26621
vchs <vchs1710@yandex.ru>
2024-10-25 18:23:54 +0300

    container with most water

---
+++ src/main/java/ru/chernov/algthms/twopointers/ContainerWithMostWater.java
@@ -0,0 +1,21 @@
+package ru.chernov.algthms.twopointers;
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] h) {
+        int len = h.length;
+        int l = 0;
+        int r = len - 1;
+        int max = 0;
+        while (l < r) {
+            max = Math.max(Math.min(h[l], h[r]) * (r - l), max);
+            if (h[l] <= h[r]) {
+                l++;
+            } else {
+                r--;
+            }
+        }
+
+        return max;
+    }
+}
---
+++ src/test/java/ru/chernov/twopointers/ContainerWithMostWaterTest.java
@@ -0,0 +1,24 @@
+package ru.chernov.twopointers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.chernov.algthms.twopointers.ContainerWithMostWater;
+
+class ContainerWithMostWaterTest {
+
+    private final ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+
+    @Test
+    void test() {
+        int result = containerWithMostWater.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+
+        Assertions.assertEquals(49, result);
+    }
+
+    @Test
+    void test2() {
+        int result = containerWithMostWater.maxArea(new int[]{1, 8, 6, 30, 5, 32, 8, 3, 7});
+
+        Assertions.assertEquals(60, result);
+    }
+}
